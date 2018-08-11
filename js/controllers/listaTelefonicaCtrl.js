1e1ca76b1a2aecfa10a9b5026c5ae8eb66ba62a4
VictorCrego <victorcrego@gmail.com>
2018-08-11 17:10:10 -0300

    Criação de Serviços

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,21 +1,29 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope) {
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI) {
     $scope.app = "Lista Telefonica";
-    $scope.contatos = [
-        {nome: "Pedro", telefone: "99999-8888", cor:"blue", data: new Date()},
-        {nome: "Ana", telefone: "99999-8877", cor:"yellow", data: new Date()},
-        {nome: "Maria", telefone: "99999-8866", cor:"red", data: new Date()}
-    ];
-    $scope.operadoras = [
-        {nome: "Oi", codigo: 14, categoria: "Celular"},
-        {nome: "Vivo", codigo: 15, categoria: "Celular"},
-        {nome: "Tim", codigo: 41, categoria: "Celular"},
-        {nome: "GVT", codigo: 25, categoria: "Fixo"},
-        {nome: "Embratel", codigo: 21, categoria: "Fixo"}
-    ];
+    $scope.contatos = [];
+    $scope.operadoras = [];
+
+    var carregarContatos = function () {
+        contatosAPI.getContatos().success(function (data) {
+            $scope.contatos = data;
+        }).error(function (data, status){
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+
+    var carregarOperadoras = function () {
+        operadorasAPI.getOperadoras().success(function (data){
+            $scope.operadoras = data;
+        });
+    };
+
     $scope.adicionarContato = function (contato) {
         $scope.contatos.push(angular.copy(contato));
-        delete $scope.contato;
-        $scope.contatoForm.$setPristine();
+        contatosAPI.saveContato(contato).success(function (data){
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        });
     };
     $scope.apagarContatos = function (contatos) {
         $scope.contatos = contatos.filter(function (contato){
